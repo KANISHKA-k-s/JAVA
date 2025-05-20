3499b40406d37344e062fbca27b35a75dfd48698
Kanishka shetty <kanishkashettty@gmail.com>
2025-05-20 16:20:21 +0530

    Add files via upload

---
+++ 1c/arrayList1c4.java
@@ -0,0 +1,17 @@
+//Write a Java program to display elements and their positions in a linked list ( using
+//l_listobj.get(p) )
+package arrayList;
+import java.util.*;
+
+public class arrayList1c4 {
+    public static void main(String[] args) {
+        LinkedList<String> list = new LinkedList<>(Arrays.asList("Red", "Green", "Blue", "Yellow"));
+
+        System.out.println("Elements with their positions:");
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println("Position " + i + ": " + list.get(i));
+        }
+    }
+
+
+}
